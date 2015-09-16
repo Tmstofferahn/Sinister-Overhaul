0043d6c12d96d1821495faabb54f742072452a0d
Tmstofferahn <tmstofferahn@gmail.com>
2015-09-16 16:25:18 -0500

    Update (9.16.2015)
    Added 3 asteroids. Still need explosion/on hit effects.
    Added script for asteroids.
    Fixed all enemy waves to have the newest version of enemies, with their
    correct shot properties.
    You can change the enemies shot patterns per wave using their setups.
    Fixed a few audio issues.

---
+++ Sinister Overhaul/Assets/Scripts/Enemy Scripts/Movements/EnemyAsteroidMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAsteroidMovement : MonoBehaviour {
+
+    public float speed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.up * -speed;
+    }
+}
